a93f84b6e289d8188df6691a4ecc2bb01f8a655d
officialarshadnoor <arshadnoor585@gmail.com>
2023-10-05 23:33:55 +0530

    trip

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = [
+    {
+        id: 1,
+        name: "Manali",
+        info: "Manali, nestled in the Indian state of Himachal Pradesh, is a breathtaking hill station renowned for its picturesque beauty. Surrounded by snow-capped mountains, lush valleys, and pristine rivers, Manali is a paradise for nature enthusiasts and adventure seekers alike. Its serene landscapes offer opportunities for trekking, skiing, and paragliding. The town itself boasts a vibrant atmosphere with bustling markets, quaint cafes, and Tibetan monasteries. Visitors can explore attractions like Rohtang Pass, Hadimba Temple, and Solang Valley, making Manali a sought-after destination for those seeking solace in the lap of the Himalayas.",
+        image: "https://www.honeymoonpackagesmanali.org/uploads/8/2/6/1/8261259/p179.png",
+        price: "4,199",
+    },
+    {
+        id: 2,
+        name: "Shimla",
+        info: "Shimla, the capital of Himachal Pradesh in India, is a charming hill station steeped in colonial history and natural beauty. Nestled amidst the Himalayas, it's famous for its cool climate, lush greenery, and picturesque landscapes. The town's British-era architecture, especially the Viceregal Lodge, adds to its old-world charm. Shimla offers a range of activities, from exploring the Mall Road's shops and eateries to visiting the Jakhu Temple and taking the toy train ride to Kalka. With its scenic beauty and cultural richness, Shimla continues to be a favorite destination for travelers seeking respite from urban life.",
+        image: "https://media.istockphoto.com/id/515816334/photo/christ-church-shimla.jpg?s=612x612&w=0&k=20&c=Im9DH1dd7h3TJmPIOZZXyCPd2fxDMOsOWe2YwZz3LW8=",
+        price: "2,999",
+    },
+    {
+        id: 3,
+        name: "Manali-Kasol",
+        info: "Manali and Kasol are two captivating destinations in Himachal Pradesh, India. Manali, nestled in the Himalayas, is a popular hill station renowned for its stunning landscapes, adventure sports, and vibrant markets. It offers a mix of natural beauty and cultural experiences with attractions like Solang Valley and Hadimba Temple.",
+        image: "https://images.unsplash.com/photo-1581791534721-e599df4417f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2Fzb2x8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+        price: "5,199",
+    },
+    {
+        id: 4,
+        name: "Dharamshala",
+        info: "Dharamshala, located in the state of Himachal Pradesh, India, is a serene hill station known for its Tibetan culture and stunning natural beauty. It serves as the residence of the 14th Dalai Lama and is often referred to as \"Little Lhasa.\" The town is divided into Upper and Lower Dharamshala, with McLeod Ganj in the upper part being the spiritual and cultural hub. Visitors can explore Tibetan monasteries, indulge in trekking amidst the Dhauladhar range, and soak in the peaceful atmosphere.",
+        image: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/06/75/bb/4e.jpg",
+        price: "2,899",
+    },
+    {
+        id: 5,
+        name: "Dalhousie",
+        info: "Dalhousie, a charming hill station in Himachal Pradesh, India, is a tranquil retreat nestled in the Dhauladhar mountain range. Named after Lord Dalhousie, a British Viceroy, the town exudes colonial-era charm with its architecture and lush landscapes. Visitors can explore picturesque trails, including the popular Dainkund Peak and Khajjiar, often called the \"Mini Switzerland of India.\" The town's serene ambiance and scenic beauty make it an ideal destination for nature enthusiasts and those seeking respite from urban life, offering a perfect blend of history and natural splendor.",
+        image: "https://assets-news.housing.com/news/wp-content/uploads/2022/09/12010206/DALHOUSIE-FEATURE-compressed.jpg",
+        price: "2,899",
+    },
+    {
+        id: 6,
+        name: "Rishikesh",
+        info: "Rishikesh, nestled in the foothills of the Himalayas in Uttarakhand, India, is a spiritual and adventure haven. It's renowned as the \"Yoga Capital of the World\" and is steeped in Hindu mythology, with the sacred Ganges River flowing through it. Rishikesh is dotted with ashrams, temples, and yoga retreats where seekers from around the globe come to find inner peace. The town also offers thrilling activities like white-water rafting, trekking, and bungee jumping, making it a unique destination that seamlessly blends spirituality with adventure amid breathtaking natural surroundings.",
+        image: "https://himalayanoutback.com/wp-content/uploads/2022/04/Interesting-Facts-About-Rishikesh.jpg",
+        price: "3,499",
+    },
+    {
+        id: 7,
+        name: "Kashmir",
+        info: "Kashmir, often referred to as \"Paradise on Earth,\" is a region in northern India characterized by its stunning natural beauty. Nestled in the Himalayas, it boasts snow-capped peaks, pristine lakes, and lush valleys. Kashmir is renowned for its breathtaking landscapes, including Dal Lake, Gulmarg's ski resorts, and Pahalgam's serene meadows. However, it has also been marred by political conflicts. Despite these challenges, Kashmir's mesmerizing beauty and rich culture continue to attract travelers who seek to experience its unique blend of natural wonders and cultural diversity.",
+        image: "https://images.unsplash.com/photo-1598091383021-15ddea10925d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2FzaG1pcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+        price: "10,999",
+    }
+];
+
+export default data;
